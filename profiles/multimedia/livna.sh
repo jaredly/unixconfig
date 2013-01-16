eefd59465e7861b08bc0f4c72a35e1ddc71dccbc
Jeff Lund <jefflund@gmail.com>
2013-01-16 15:30:01 -0700

    both rpmfusion and livna are in multimedia

---
+++ profiles/multimedia/livna.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo
+echo "** Beginning the script $0"
+echo
+
+while getopts "ksy" options
+do
+    case $options in
+        s) SHORT="true" ;;
+        ?) ;;
+    esac
+done
+
+if [[ ! -z $SHORT ]]; then
+    exit 0
+fi
+
+##############################################################################
+# Livna Repository
+
+if ! rpm -q livna-release; then
+    rpm -Uvh "http://rpm.livna.org/livna-release.rpm"
+fi
+
