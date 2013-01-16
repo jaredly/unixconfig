467da0e0d29ef0d2cdf9a907d9b8bd5c7d76a6be
Andrew McNabb <amcnabb@mcnabbs.org>
2013-01-16 13:55:53 -0700

    Add a profile for disabling the persistent-net feature

---
+++ profiles/no-persistent-net/no-persistent-net.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Get rid of annoying persistent-net and biosdevname stuff that makes hell
+# break loose if you switch ethernet cards between boots (at the cost of
+# some minor inconvenience for machines with two ethernet cards).
+
+echo
+echo "** Beginning the script $0"
+echo
+
+rm -f /etc/udev/rules.d/*persistent-net*
+
+# Mask the persistent net generator rule.
+ln -s /dev/null /etc/udev/rules.d/75-persistent-net-generator.rules
+
+# Note that ifcfg-eth0.old gets in the way.
+rm -f /etc/sysconfig/network-scripts/ifcfg-*.old
+
+# Get rid of biosdevname-based net config files that might have slipped in.
+rm -f /etc/sysconfig/network-scripts/ifcfg-em*
+rm -f /etc/sysconfig/network-scripts/ifcfg-p*
