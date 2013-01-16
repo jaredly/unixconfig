139beb93f79c42343ce5cdf0c6a132f803af5e42
Andrew McNabb <amcnabb@mcnabbs.org>
2013-01-16 15:36:02 -0700

    Fix the number of ..'s in etc-links.sh

--- lib/etc-links.sh
+++ lib/etc-links.sh
@@ -4,7 +4,7 @@ echo
 echo "** Beginning the script $0"
 echo
 
-. "$(dirname "$0")"/../../../lib/symlink.sh
-local_etc="$(readlink_f "$(dirname "$0")/../etc")"
+. "$(dirname "$0")"/../../lib/symlink.sh
+local_etc="$(readlink_f "$(dirname "$0")/etc")"
 
 symlink "..$local_etc" /etc
